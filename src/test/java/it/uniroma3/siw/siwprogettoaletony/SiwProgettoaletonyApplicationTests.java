844e5098bee9febfa935cbb3f3fa961960b83b27
Anto2134 <ant.cotugno1@stud.uniroma3.it>
2024-05-20 16:32:58 +0200

    creazione

---
+++ src/test/java/it/uniroma3/siw/siwprogettoaletony/SiwProgettoaletonyApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwprogettoaletony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwProgettoaletonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
